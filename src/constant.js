f8a33589f9d47e0acd7f75431955f6d79f18f96e
afham123 <afhamfardeen98@gmail.com>
2024-11-06 09:11:51 +0530

    fix server url

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 module.exports = {
     API_SECRET_KEY: "lZDAwMWYtNTFiYi00NzVhLTgxZDAtMDjoxNTE5",
     API_SECRET_DATA: "VudGl0eSI6eyJlbWFpbCI6InQzcXVsZmVlbUBrd2l2NS42dXIiLCJuYW1lIjoiM2lvCI6ZmFsc2UsInR5cGU",
-    server_url: "http://43.204.100.254:3001",
-    // server_url: "http://localhost:3001",
-    noDataFound : "To Data found to  display"
+    server_url: "https://srm.mahavir-agencies.com/api",
+    // server_url: "http://localhost:3001/api",
+    noDataFound : "No Data found to display"
 }
